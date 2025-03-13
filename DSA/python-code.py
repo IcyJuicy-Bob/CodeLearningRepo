4b5dad3c32a7e649d80abe5c4066a44f03d102c9
20roech <20roech@arthurmellows.net>
2025-03-13 11:25:47 +0000

    dsa update

--- DSA/python-code.py
+++ DSA/python-code.py
@@ -13,20 +13,35 @@ for fibo in range(18):            #repeats for 18 times basically
     prev1 = newFibo               #this and previous changes the previous numbers to the next numbers
 
 #fibonacci using recursion
+#recursion is calling itself inside itself
 print(0)
 print(1)
 count = 2
-#comment on this i havent don eit yet im more intrested iin pokemon
-def fibonacci(prev1, prev2):
-    global count
-    if count <= 19:
+
+def fibonacci(prev1, prev2): #defines a function with the name fibonacci
+    global count #defines it uses a global variable 
+    if count <= 19: # if count is less or equal to 19
         newFibo = prev1 + prev2
         print(newFibo)
         prev2 = prev1
         prev1 = newFibo
         count += 1
-        fibonacci(prev1, prev2)
+        fibonacci(prev1, prev2) #because it calls itself inside itself it will repeat uptil its told not to which is when the count is bigger than 19
     else:
         return
 
 fibonacci(1,0)
+
+#finding the n'th fibonnaci number using recursion
+def F(n): #define function with name f
+    if n <= 1: #if n is equal or less than 1 return n 
+        return n
+    else:
+        return F(n - 1) + F(n - 2) #else return the function of n-1 plus n-2
+
+print(F(19)) #print function 19 bascially the 20th number because in python the first number is 0 its called a 0 based index
+
+
+#arrays
+#this is a list but can be used in the same way as a array in some aspects
+myarray = [7, 12, 9, 2, 15]
