728e5fe3213ca84aa7b26cc9dcfe01bb64663bb0
20roech <20roech@arthurmellows.net>
2025-05-13 08:45:07 +0000

    SQL revision document for exam

---
+++ SQL/SQLcode.sql
@@ -0,0 +1,26 @@
+/*this is a comment in a sql document i will be using this to comment throughout the sql file*/
+/*sql stands for structured query language*/
+/*
+SQL can execute queries against a database
+SQL can retrieve data from a database
+SQL can insert records in a database
+SQL can update records in a database
+SQL can delete records from a database
+SQL can create new databases
+SQL can create new tables in a database
+SQL can create stored procedures in a database
+SQL can create views in a database
+SQL can set permissions on tables, procedures, and views
+*/
+/*SQL is a ANSI/ISO standard but there are diffrent types of SQL 
+to be compliant to the ANSI standard they need to have the major commands
+SELECT, UPDATE, DELETE, INSERT, WHERE in a similar manner*/
+
+/*to use a database in a website you need a RDBMS(e.g SQL server/MySQL), a server side language (e.g php)
+use sql to get the data you want and html and css to style the website*/
+
+/*RDBMS*/
+/*RDBMS stands for Relational Database Management System.*/
+/*a RDBMS is the basis of all database systems e.g MySQL*/
+/*the data in RDBMS is stored in objects called tables*/
+/*a table is a collection of related data entries in collums and rows*/
