e7249191107122e4b2f504e7466aa8f902ae9d5d
20roech <20roech@arthurmellows.net>
2025-05-13 15:07:19 +0000

    SQL update

--- SQL/SQLcode.sql
+++ SQL/SQLcode.sql
@@ -24,3 +24,27 @@ use sql to get the data you want and html and css to style the website*/
 /*a RDBMS is the basis of all database systems e.g MySQL*/
 /*the data in RDBMS is stored in objects called tables*/
 /*a table is a collection of related data entries in collums and rows*/
+/*a field is a column of the table going vertically but is like the header like it may say cities and then in each row under is has diffrent city names*/
+/*a record is a row of the table going horizontially for every entry for example might hold a name then in another field/column it has age then height ect going horizontally*/
+/*going down on this hypothetical table would be other peoples names hights ages ect all in a different record*/
+/*a column is just a row going vertically down like record just vertical*/
+
+SELECT * FROM customers;
+/*this selects all records from the customers table*/
+/*it SELECTs the * means all FROM the customers table*/
+/*the semi-colon is the standard way to seperate sql statements if the database system can handle more than one statement in the same call to the server*/
+
+/*databases normally contain more than 1 table*/
+/*SQL keywords aren't case sensitive SELECT is the same as select*/
+
+SELECT - /*extracts data from a database*/
+UPDATE - /*updates data in a database*/
+DELETE - /*deletes data from a database*/
+INSERT INTO - /*inserts new data into a database*/
+CREATE DATABASE - /*creates a new database*/
+ALTER DATABASE - /*modifies a database*/
+CREATE TABLE - /*creates a new table*/
+ALTER TABLE - /*modifies a table*/
+DROP TABLE - /*deletes a table*/
+CREATE INDEX - /*creates an index (search key)*/
+DROP INDEX - /*deletes an index*/
